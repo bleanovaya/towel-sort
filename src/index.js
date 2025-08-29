4961e4ad47a78eaf6695c988662c1fff25b4ca91
bleanovaya <lina.otherside666@gmail.com>
2025-08-29 05:12:46 +0300

    feat: implement towelSort algorithm with alternating row direction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix) || matrix.length === 0) {
+    return [];
+  }
+
+  return matrix
+    .map((row, index) => (index % 2 === 0 ? row : row.slice().reverse()))
+    .flat();
 };
